7dcf052742ef7e8e1391624e99ba5210a2d2d38e
michael71161 <michael71161@gmail.com>
2025-06-22 13:57:03 +0300

    version to submition

--- scripts/index.js
+++ scripts/index.js
@@ -29,15 +29,11 @@ console.log(initialCards);
 
 /* elements */
 
-const profileEditButton = document.querySelector(
-  "#profile-edit-button"
-); /*because we used id*/
+const profileEditButton = document.querySelector("#profile-edit-button");
 
 const profileEditModal = document.querySelector("#profile-edit-modal");
-console.log(profileEditButton);
 
 const profileEditModalClose = document.querySelector("#profile-edit-close");
-console.log(profileEditModalClose); /*we selecting the close modal button*/
 
 /*add card*/
 const addCardModal = document.querySelector("#add-card-modal");
@@ -48,41 +44,34 @@ const addCardCloseButton = document.querySelector("#add-card-close");
 
 const addCardSubmitButton = document.querySelector("#submit-image");
 
-/*now we want to grab profile info (title and description)*/
+/*get profile elements*/
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 
-/*checking what we grabbed see the console.log with textContent to grab text only*/
-
-console.log(profileTitle.textContent);
-console.log(profileDescription.textContent);
-
-/*no we want to grab the inputs*/
+/*get inputs elements*/
 const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 
-/*we grabbing add card inputs*/
-
 const cardTitleInput = document.querySelector("#card-title-input");
 const cardLinkInput = document.querySelector("#card-link-input");
 
-/*now we want that from the modal it will go yo the html, to edit our form*/
+/*get form modal element*/
 
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 const cardListEL = document.querySelector(".cards__list");
-/*grabing the form from the modal*/
 
-/*adding func to close popup instead of using classlist.remove "modal__opened"*/
+/*image popup selectors*/
+const imageModal = document.querySelector("#image-modal");
+const imageModalClose = document.querySelector("#image-modal-close");
+const imageModalImage = imageModal.querySelector(".image__modal_image");
+const imageModalCaption = imageModal.querySelector(".image__modal_caption");
 
-/*we erased the hardcoded cards and now need to grab the template element*/
-/*also notice that we need the **content itself** and we need the **card content**
- which is the first nested element of card template id*/
-/*also we could use another query*/
+/*get card template*/
+
 const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
-console.log(cardTemplate);
 
 /* functions */
 
@@ -104,7 +93,13 @@ function getCardElement(cardData) {
     cardElement.remove();
   });
 
-  console.log(cardImageEl);
+  cardImageEl.addEventListener("click", () => {
+    imageModalImage.src = cardData.link;
+    imageModalImage.alt = cardData.name;
+    imageModalCaption.textContent = cardData.name;
+    imageModal.classList.add("image__modal_opened");
+  });
+
   cardImageEl.src = cardData.link;
   cardTitleEl.textContent = cardData.name;
   cardImageEl.alt = cardData.name;
@@ -123,37 +118,36 @@ function handleAddCardSubmit() {
     link: cardLinkInput.value,
     name: cardTitleInput.value,
   });
-  cardListEL.append(newCard);
+  cardListEL.prepend(newCard);
   addCardModal.classList.remove("modal_opened");
 }
 
 /* event listeners*/
-
-/* now we want to submit the form and prevent default which is refreshing of the whole page */
-/* we will pass the title and the description to the page through the form */
-/* e stands for event, we going to use a callback function */
 
 profileEditForm.addEventListener("submit", handleProfileEditSUbmit);
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add(
-    "modal_opened"
-  ); /*adding class for the modal to make it open, class modal opened*/
-}); /*"click" is the event second argument is function*/
+  profileEditModal.classList.add("modal_opened");
+});
 
 /*open the add card modal*/
 addCardButton.addEventListener("click", () => {
   addCardModal.classList.add("modal_opened");
 });
 
+/*add card to the DOM*/
 addCardSubmitButton.addEventListener("click", (e) => {
   e.preventDefault();
   handleAddCardSubmit();
 });
 
-/*close the add card modal*/
+/*close  modals*/
+imageModalClose.addEventListener("click", () => {
+  imageModal.classList.remove("image__modal_opened");
+});
+
 addCardCloseButton.addEventListener("click", () => {
   addCardModal.classList.remove("modal_opened");
 });
@@ -164,5 +158,3 @@ initialCards.forEach((cardData) => {
   const cardElement = getCardElement(cardData);
   cardListEL.append(cardElement);
 });
-
-/**Jacques Cousteau  Explorer */
